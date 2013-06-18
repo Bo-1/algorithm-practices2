b04a484c00cba5d7642d892a74820e10f656a696
boplusplus <bo.chen.cs@gmail.com>
2013-06-18 18:24:02 +0100

    better version

--- reverseInteger.java
+++ reverseInteger.java
@@ -1,23 +1,17 @@
 public class Solution {
     public int reverse(int x) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
         if(x == 0)
             return 0;
         int sign = x > 0 ? 1 : -1;
-        int base = 10;
-        int[] digits = new int[10];
         int last = 0;
         x = Math.abs(x);
-        for(int i = 9; i >= 0; i--) {
-            digits[i] = (x % base - last) * 10 / base;
-            last = x % base;
-            base *= 10;
-        }
-        int i = 0;
-        while(digits[i] == 0)
-            i++;
         long result = 0;
-        for(int j = i; j < 10; j++) {
-            result += digits[j] * Math.pow(10, j - i);  
+        while( x > 0 ) {
+            int digit = x % 10;
+            result = result * 10 + digit;
+            x = x / 10;
         }
         if(result > Integer.MAX_VALUE)
             return sign * Integer.MAX_VALUE;
