667edbc0b81a67af2c1e620e26a34a990510a4e3
boplusplus <bo.chen.cs@gmail.com>
2013-06-26 00:04:05 +0100

    26/06

--- reverseInteger.java
+++ reverseInteger.java
@@ -5,18 +5,18 @@ public class Solution {
         if(x == 0)
             return 0;
         int sign = x > 0 ? 1 : -1;
-        int last = 0;
-        x = Math.abs(x);
         long result = 0;
-        while( x > 0 ) {
-            int digit = x % 10;
-            result = result * 10 + digit;
+        while(x != 0) {
+            result = result * 10 + x % 10;
             x = x / 10;
         }
-        if(result > Integer.MAX_VALUE)
-            return sign * Integer.MAX_VALUE;
+        if(result > Integer.MAX_VALUE) {
+            if(sign > 0)
+                return Integer.MAX_VALUE;
+            else
+                return Integer.MIN_VALUE;
+        }
         else
-            return sign * (int)result;
-        
+            return (int)result;
     }
 }
--- searchInsertPosition.java
+++ searchInsertPosition.java
@@ -2,20 +2,19 @@ public class Solution {
     public int searchInsert(int[] A, int target) {
         // Start typing your Java solution below
         // DO NOT write main() function
+        if(A.length == 0 ||  A == null)
+            return -1;
         return binarySearch(A, target, 0, A.length - 1);
     }
-//remember always return start    
+    //user length - 1, return start
     public int binarySearch(int[] A, int target, int start, int end) {
-            if(start <= end) {
-                int mid = start + (end - start) / 2;
-                if(target == A[mid])
-                    return mid;
-                else if(A[mid] > target)
-                    return binarySearch(A, target, start, mid - 1);
-                else
-                    return binarySearch(A, target, mid + 1, end);
-            }
-            else
-                return start;
+        if(start > end)
+            return start;
+        int mid = start + (end - start)/ 2;
+        if(A[mid] == target)
+            return mid;
+        else if(A[mid] < target)
+            return binarySearch(A, target, mid + 1, end);
+        else
+            return binarySearch(A, target, start, mid - 1);
     }
-}
